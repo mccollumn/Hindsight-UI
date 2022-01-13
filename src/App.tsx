7c023ed0d4cb911b49204b2e9e1e03127ea4391e
Nick McCollum <nmccollum81@gmail.com>
2022-01-12 18:04:52 -0800

    Yo

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React... NOW!
+          Learn React... NOW! YO!
         </a>
       </header>
     </div>
