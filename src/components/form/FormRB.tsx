1f24b0b59e080b0113731670066b961dbcd40ff3
Dominic <scientist1882@gmail.com>
2022-08-31 12:08:08 -0700

    17 Initial Form

---
+++ src/components/form/FormRB.tsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import {
+  FormProvider,
+  useForm,
+  UseFormReturn,
+  UseFormReset
+} from 'react-hook-form';
+import { Box, BoxProps } from '@mui/material';
+
+export const FormRB = ({
+  methods,
+  defaultValues,
+  children,
+  className,
+  onSubmit = () => { },
+  ...props
+}: FormRBProps) => {
+  let formMethods = useForm({
+        defaultValues
+    });
+
+    // Use form methods from parent
+    if (methods) {
+      formMethods = methods;
+    }
+
+  const submitHandler = (values: any) => {
+    onSubmit(values, formMethods.reset);
+  }
+
+  const classNames = ['rb-form', className];
+
+  return (
+    <FormProvider {...formMethods}>
+      <form
+        noValidate
+        onSubmit={formMethods.handleSubmit(submitHandler)}>
+
+        <Box
+          className={classNames.filter(f => f).join(' ')}
+          {...props}>
+
+          {children}
+
+        </Box>
+
+      </form>
+    </FormProvider >
+  );
+
+}
+
+export interface FormRBProps extends BoxProps {
+  /**
+   * Form submit handler
+   * Returns reset form function to reset to default values
+   */
+  onSubmit?: (data: any, reset?: UseFormReset<any>) => void,
+  /** 
+   * Default object in the structure of the form
+   */
+  defaultValues?: Object,
+  /** 
+   * CSS display (EX. grid, flex, block)
+   */
+  display?: string,
+  /** 
+   * Add unique className
+   */
+  className?: string,
+  /**
+   * Styling on <form/> element
+   */
+  style?: Object,
+  /**
+   * Extend Material styles sent to useStyles() hook
+   */
+  customStyles?: Object
+  /**
+   * All methods from React-Hook-Forms for this form instance
+   */
+  methods?: UseFormReturn,
+  /** 
+   * All children
+   */
+  children: any,
+}
