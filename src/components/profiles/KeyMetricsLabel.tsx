23fc689579fac07369176c6e053f0efe98ca548f
Nick McCollum <nmccollum81@gmail.com>
2022-11-02 21:49:09 +0000

    Added KeyMetrics page

---
+++ src/components/profiles/KeyMetricsLabel.tsx
@@ -0,0 +1,20 @@
+import Box from "@mui/material/Box";
+
+const KeyMetricsLabel = () => {
+  return (
+    <Box
+      sx={{
+        position: "absolute",
+        right: "10%",
+        top: "50%",
+        transform: "translateY(-50%)",
+      }}
+    >
+      Views: 10
+      <br></br>
+      Visits: 1
+    </Box>
+  );
+};
+
+export default KeyMetricsLabel;
