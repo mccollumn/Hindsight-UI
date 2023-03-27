140b4a2d569430ec8ba684456017ce4e82efd12a
Nick McCollum <mccollumn@webtrends.com>
2023-03-27 18:36:51 +0000

    Styled grid

--- src/components/data-vis/DataTable.tsx
+++ src/components/data-vis/DataTable.tsx
@@ -9,50 +9,50 @@ import { GridApiPremium } from "@mui/x-data-grid-premium/models/gridApiPremium";
 import { DEFAULT_TABLE_HEIGHT } from "../../constants/constants";
 
 const StyledDataGrid = styled(DataGridPremium)(({ theme }) => ({
-  //   border: 0,
-  //   color:
-  //     theme.palette.mode === "light"
-  //       ? "rgba(0,0,0,.85)"
-  //       : "rgba(255,255,255,0.85)",
-  //   fontFamily: [
-  //     "-apple-system",
-  //     "BlinkMacSystemFont",
-  //     '"Segoe UI"',
-  //     "Roboto",
-  //     '"Helvetica Neue"',
-  //     "Arial",
-  //     "sans-serif",
-  //     '"Apple Color Emoji"',
-  //     '"Segoe UI Emoji"',
-  //     '"Segoe UI Symbol"',
-  //   ].join(","),
-  //   WebkitFontSmoothing: "auto",
-  //   letterSpacing: "normal",
-  //   "& .MuiDataGrid-columnsContainer": {
-  //     backgroundColor: theme.palette.mode === "light" ? "#fafafa" : "#1d1d1d",
-  //   },
-  //   "& .MuiDataGrid-iconSeparator": {
-  //     display: "none",
-  //   },
-  //   "& .MuiDataGrid-columnHeader, .MuiDataGrid-cell": {
-  //     borderRight: `1px solid ${
-  //       theme.palette.mode === "light" ? "#f0f0f0" : "#303030"
-  //     }`,
-  //   },
-  //   "& .MuiDataGrid-columnsContainer, .MuiDataGrid-cell": {
-  //     borderBottom: `1px solid ${
-  //       theme.palette.mode === "light" ? "#f0f0f0" : "#303030"
-  //     }`,
-  //   },
-  //   "& .MuiDataGrid-cell": {
-  //     color:
-  //       theme.palette.mode === "light"
-  //         ? "rgba(0,0,0,.85)"
-  //         : "rgba(255,255,255,0.65)",
-  //   },
-  //   "& .MuiPaginationItem-root": {
-  //     borderRadius: 0,
-  //   },
+  border: 0,
+  color:
+    theme.palette.mode === "light"
+      ? "rgba(0,0,0,.85)"
+      : "rgba(255,255,255,0.85)",
+  fontFamily: [
+    "-apple-system",
+    "BlinkMacSystemFont",
+    '"Segoe UI"',
+    "Roboto",
+    '"Helvetica Neue"',
+    "Arial",
+    "sans-serif",
+    '"Apple Color Emoji"',
+    '"Segoe UI Emoji"',
+    '"Segoe UI Symbol"',
+  ].join(","),
+  WebkitFontSmoothing: "auto",
+  letterSpacing: "normal",
+  "& .MuiDataGrid-columnsContainer": {
+    backgroundColor: theme.palette.mode === "light" ? "#fafafa" : "#1d1d1d",
+  },
+  "& .MuiDataGrid-iconSeparator": {
+    display: "none",
+  },
+  "& .MuiDataGrid-columnHeader, .MuiDataGrid-cell": {
+    borderRight: `1px solid ${
+      theme.palette.mode === "light" ? "#f0f0f0" : "#303030"
+    }`,
+  },
+  "& .MuiDataGrid-columnsContainer, .MuiDataGrid-cell": {
+    borderBottom: `1px solid ${
+      theme.palette.mode === "light" ? "#f0f0f0" : "#303030"
+    }`,
+  },
+  "& .MuiDataGrid-cell": {
+    color:
+      theme.palette.mode === "light"
+        ? "rgba(0,0,0,.85)"
+        : "rgba(255,255,255,0.65)",
+  },
+  "& .MuiPaginationItem-root": {
+    borderRadius: 0,
+  },
 }));
 
 const DataTable = ({ config, apiRef, ...props }: DataTableProps) => {
