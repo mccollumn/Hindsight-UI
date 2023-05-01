b043ad2d2782800c58da86bdaab0d08724947f66
Nick McCollum <mccollumn@webtrends.com>
2023-05-01 23:25:10 +0000

    Initial commit for installer. Still needs a deploy step and to be tested.

---
+++ installer/build_installer.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+InstallerOutputDir="./installer/Output"
+InstallerAppDir="${InstallerOutputDir}/app"
+InstallerMsiDir="${InstallerOutputDir}/msi"
+InnoSetupDir="./installer/Inno Setup 6"
+InnoScriptName="wtrui.iss"
+
+echo "*** Building Installer ***"
+
+# Get app version
+while getopts v: flag
+do
+    case "${flag}" in
+        v) version=${OPTARG};;
+    esac
+done
+echo "Version: ${version}"
+
+# Clean up the installer output directory
+echo -e "\nCleaning up files in ${InstallerOutputDir}..."
+rm -rfv ${InstallerOutputDir}
+
+# Compile the installer
+echo -e "\nCompiling Inno Setup script ${InnoScriptName}"
+"${InnoSetupDir}/iscc.exe" /O"${InstallerOutputDir}" /F"wtr_setup${version}" "./installer/${InnoScriptName}"
+
+if [ $? -ne 0 ]
+then
+    echo "Failed to compile ${InnoScriptName}" >&2
+    exit 1
+fi
+
+# Copy files to installer output directory
+echo -e "\nCopying latest build into installer..."
+mkdir -vp ${InstallerAppDir}
+mkdir -vp ${InstallerMsiDir}
+cp -rv ./build/* ${InstallerAppDir}
+cp -v ./installer/web.config ${InstallerAppDir}
+cp -v ./installer/readme.txt ${InstallerOutputDir}
+cp -v ./installer/rewrite_amd64_en-US.msi ${InstallerMsiDir}
+rm -v ${InstallerAppDir}/mockServiceWorker.js
