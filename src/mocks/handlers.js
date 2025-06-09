08e40b73ba8545e0b0eb379a7dad7c2f139598dc
Nick McCollum <nmccollum81@gmail.com>
2025-06-09 09:45:54 -0700

    Updated API endpoint URL in handlers.js

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -3,7 +3,7 @@ import { pages } from "./data/daily";
 
 export const handlers = [
   rest.get(
-    "http://usw2op19s08w:8080/v2_0/ReportService/profiles/*/reports/*/",
+    "http://localhost:8080/v2_0/ReportService/profiles/*/reports/*/",
     (req, res, ctx) => {
       const date = req.url.searchParams.get("start_period");
       const day = date.match(/\d+$/).pop();
